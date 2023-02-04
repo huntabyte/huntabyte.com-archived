f804bf1c441493719468b9f02c58c8f85a65dc91
Hunter Johnston <johnstonhuntera@gmail.com>
2023-02-03 20:19:56 -0500

    add changed files step

--- src/routes/api/webhooks/refresh-content/+server.ts
+++ src/routes/api/webhooks/refresh-content/+server.ts
@@ -4,14 +4,19 @@ import { env } from "$env/dynamic/private"
 export const POST: RequestHandler = async ({ request }) => {
 	const authorization = request.headers.get("Authorization")
 	if (!authorization) {
+		console.error("/api/webhooks/refresh-content: Missing authorization header")
 		return json({ error: "Missing authorization header" }, { status: 400 })
 	}
 	const authCreds = Buffer.from(authorization.split(" ")[1], "base64").toString(
 		"ascii",
 	)
 	if (authCreds !== env.REFRESH_WEBHOOK_AUTH) {
+		console.error("/api/webhooks/refresh-content: Invalid authorization header")
 		return json({ error: "Invalid authorization header" }, { status: 400 })
 	}
 
-	return json("hit webhook")
+	const body = await request.json()
+	console.error("body:", body)
+
+	return json({ message: "success" })
 }
