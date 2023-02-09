f8c8c3c6a8607a693ecfc1ad01d0446baac1786f
Hunter Johnston <64506580+huntabyte@users.noreply.github.com>
2023-02-08 19:46:41 -0500

    add custom server for region testing

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import { createServer } from "http"
+import { handler } from "../build/handler.js"
+const port = 8081
+const app = express()
+const server = createServer(app)
+
+app.use((req, res, next) => {
+	if (req.query.region) {
+		console.log("Requested region:", req.query.region)
+		res.setHeader("fly-replay", `region=${req.query.region}`)
+	}
+	console.log("Currently in region:", process.env.FLY_REGION)
+	next()
+})
+
+app.use(handler)
+
+server.listen(port)
